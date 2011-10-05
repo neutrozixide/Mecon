d3dae8639eb6ef27b1577930c8d7ed03eada0758
Priyank Patel <scientiaesthete@gmail.com>
2011-10-04 20:08:50 -0700

    Added Octree class.

---
+++ Octree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mecon
+{
+    /// <summary>
+    /// Provides data structure for storing voxel data in the form of
+    /// sparse octree in order to save memory and increase spatial
+    /// processing speed.
+    /// </summary>
+    /// <remarks>
+    /// Specific octree structure to be determined.
+    /// See http://plum.eecs.jacobs-university.de/download/icat2011_1.pdf
+    /// </remarks>
+    class Octree
+    {
+
+    }
+}
